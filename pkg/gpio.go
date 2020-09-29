e9022a3c1f73bbd97576382fefe6aaa09f7ca16b
nolte <nolte07@googlemail.com>
2020-09-29 21:51:16 +0200

    print error

--- pkg/gpio.go
+++ pkg/gpio.go
@@ -1,15 +1,25 @@
 package pkg
 
-import "github.com/stianeikeland/go-rpio"
+import (
+	"fmt"
+	"os"
 
-func ControllGIPO() error {
-	err := rpio.Open()
-	if err != nil {
-		return err
+	"github.com/stianeikeland/go-rpio"
+)
+
+func ControllGIPO() {
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
+	// Unmap gpio memory when done
+	defer rpio.Close()
 	pin := rpio.Pin(2)
 
+	// Set pin to output mode
+	pin.Output()
+
 	pin.Toggle()
-	return nil
 }
